13e0edf2bab0552fb7c788d722ae5c04485b5b50
Kantai <kantai.developer@gmail.com>
2021-08-26 09:25:27 +0800

    Add configuration to config/services.php

--- config/services.php
+++ config/services.php
@@ -79,4 +79,10 @@ return [
         'client_secret' => env('TWITTER_CLIENT_SECRET'),
         'redirect' => env('TWITTER_REDIRECT'),
     ],
+
+    'line' => [
+        'client_id' => env('LINE_CLIENT_ID'),
+        'client_secret' => env('LINE_CLIENT_SECRET'),
+        'redirect' => env('LINE_REDIRECT_URI')
+    ],
 ];
