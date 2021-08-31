9426b843ccdb8c003820798a46e2b15bb174087a
Kantai <kantai.developer@gmail.com>
2021-08-31 09:07:47 +0800

    Update WebhookController.php

--- app/Domains/Chat/Http/Controllers/Frontend/WebhookController.php
+++ app/Domains/Chat/Http/Controllers/Frontend/WebhookController.php
@@ -42,17 +42,21 @@ class WebhookController
     public function index(Request $request)
     {
         $events = $request->all();
-        foreach ($events['events'] as $event) {
-            $this->eventService->store([
-                'destination' => $events['destination'],
-                'type' => $event['type'],
-                'response' => $event,
-            ]);
+        if (isset($events['events']) && is_array($events['events'])) {
+            foreach ($events['events'] as $event) {
+                $this->eventService->store([
+                    'destination' => $events['destination'] ?? null,
+                    'type' => $event['type'] ?? null,
+                    'response' => $event,
+                ]);
 
-            if (!$this->userService->findByUserId($event['source']['userId'])) {
-                $response = Http::withToken(config('line.channel.access_token'))
-                                ->get(sprintf('https://api.line.me/v2/bot/profile/%s', $event['source']['userId']));
-                $this->userService->store($response->json());
+                if (isset($event['source']) && isset($event['source']['userId'])) {
+                    if (!$this->userService->findByUserId($event['source']['userId'])) {
+                        $response = Http::withToken(config('line.channel.access_token'))
+                            ->get(sprintf('https://api.line.me/v2/bot/profile/%s', $event['source']['userId']));
+                        $this->userService->store($response->json());
+                    }
+                }
             }
         }
 
